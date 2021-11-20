fb48e3a6b1974e9903622e2fbd54a7a564887c32
anandaregita_10 <anandaregita_05@gmail.com>
2021-11-20 18:27:58 +0700

    ukk_oop

---
+++ betamart_crud/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace betamart_crud
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\user\Documents\databasedb_beta mart.accdb");
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from data_barang";
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into data_barang values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("record inserted successfully");
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            string query = "delete from data_barang where kode=" + textBox1.Text + "";
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            MessageBox.Show("Barang berhasil dihapus");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Don't Blank!");
+            }
+            else {
+                con.Open();
+                OleDbCommand cmd = con.CreateCommand();
+                string query = "update data_barang set nama='" + textBox2.Text + "',harga='" + textBox3.Text + "' ,kategori='" + textBox4.Text + "' where kode=" + textBox1.Text + "";
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                MessageBox.Show("Item has been Edited");
+             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from data_barang where nama='" + textBox5.Text + "' ";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            count = Convert.ToInt32(dt.Rows.Count.ToString());
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+
+            if (count == 0)
+            {
+                MessageBox.Show("Barang tidak ditemukan");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+    }
+}
